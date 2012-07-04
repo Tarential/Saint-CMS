7caf6a35165d704fa564c21243337f2593a33bd4
Preston St. Pierre <webmaster@aparadine.com>
2012-07-04 06:48:08 -0700

    Upgraded sitemap index display to work recursively.

--- core/blocks/index/index.php
+++ core/blocks/index/index.php
@@ -1,10 +1,13 @@
-<?php foreach (Saint::getIndex() as $i): ?>
-	<h4><a href="<?php echo SAINT_URL . $i[0]; ?>"><?php echo $i[1]; ?></a></h4>
-	<?php if (sizeof($i[3])): ?>
-	<div class="subindex">
-	<?php foreach ($i[3] as $si): ?>
-		<h4><a href="<?php echo SAINT_URL . $si[0]; ?>"><?php echo $si[1]; ?></a></h4>
-	<?php endforeach; ?>
-	</div>
+<?php function displayItem($item) { ?>
+	<h4><a href="<?php echo SAINT_URL . $item[0]; ?>"><?php echo $item[1]; ?></a></h4>
+	<?php if (sizeof($item[3])): ?>
+		<div class="subindex">
+		<?php foreach ($item[3] as $subitem) {
+			 displayItem($subitem);
+		} ?>
+		</div>
 	<?php endif; ?>
-<?php endforeach; ?>
+<?php } ?>
+<?php foreach (Saint::getIndex() as $i) {
+	displayItem($i);
+} ?>
--- core/blocks/index/xml.php
+++ core/blocks/index/xml.php
@@ -1,10 +1,18 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'."\n"; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-<?php foreach (Saint::getIndex() as $i): ?>
+<?php function displayItem($item) { ?>
 	<url>
-		<loc><?php echo $i[0]; ?></loc>
-		<lastmod><?php echo date("Y-m-d",strtotime($i[2])); ?></lastmod>
+		<loc><?php echo $item[0]; ?></loc>
+		<lastmod><?php echo date("Y-m-d",strtotime($item[2])); ?></lastmod>
 	</url>
-<?php endforeach; ?>
+	<?php if (sizeof($item[3])) {
+		foreach ($item[3] as $subitem) {
+		 displayItem($subitem);
+		}
+	}
+} ?>
+<?php foreach (Saint::getIndex() as $i) {
+	displayItem($i);
+} ?>
 </urlset>
